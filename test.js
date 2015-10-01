d75fdb3986d3ee89ca9422a16f02d14eaabebdc8
Chintan Radia <radiachints1512@gmail.com>
2015-10-01 15:41:02 +0530

    tests

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var childProcess = require('child_process');
 
 test('User name validation check', function (t) {
 	t.plan(1);
-	childProcess.execFile('./cli.js', [''], {cwd: __dirname}, function (err, stdout) {
+	childProcess.execFile('./cli.js', {cwd: __dirname}, function (err, stdout) {
 		t.equal('User name required', stdout, 'Both strings are equal');
 	});
 });
