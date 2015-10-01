613ebd973f687316245d8b957ca3f489e15340aa
Chintan Radia <radiachints1512@gmail.com>
2015-10-01 09:40:01 +0000

    tweaks

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # github-user-publickey
 Get GitHub users PublicKey
+
+## Install
+
+`> npm install --save github-user-publickey`
+
+## Usage
+
+```js
+var githubUserPublicKey = require('github-user-publickey');
+githubUserPublicKey(cli.input[0], function (err, data) {
+    console.log(data);
+    //=>[ssh-rsa AAA....]
+});
+```
+
+## CLI
+
+`> npm install --global github-user-publickey`
+
+```
+$ github-user-publickey --help
+
+  Example
+    github-user-publickey beat
+    ssh-rsa AA....
+```
+
+## License
+
+MIT © [Chintan Radia](https://github.com/beatfreaker)
--- cli.js
+++ cli.js
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
 'use strict';
+var meow = require('meow');
 var githubUserPublicKey = require('./');
 
-githubUserPublicKey('beatfreaker', function (err, data) {
+var cli = meow({
+	help: [
+		'Example',
+		'  $ github-user-publickey beat',
+		'  ssh-rsa AAA.....'
+	]
+});
+
+if (!cli.input.length) {
+	console.error('User name required');
+	process.exit(1);
+}
+
+githubUserPublicKey(cli.input[0], function (err, data) {
+    if (err) {
+		console.error(err.message);
+		process.exit(1);
+	}
+    
     for (var i = 0; i < data.length; i++) {
         console.log(i + 1 + ') ' + data[i]);
     }
